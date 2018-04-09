241d4b1fc37925406b192562faed5a13910bf6cb
TrisnaRamadhanti <TrisnaRamadhanti@gmail.com>
2018-04-09 22:15:58 +0700

    updateContact.php

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -4,10 +4,10 @@
     class Contact extends CI_Controller 
     {
     
-        public function     
+        public function index()    
         {
             
-            this->load->view('contact');
+            $this->load->view('contact');
             
         }
     
